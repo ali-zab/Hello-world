6f8543bc61da9e7764c48b6867034b7ff3525ba2
ali-zab <78250762+ali-zab@users.noreply.github.com>
2021-02-01 12:07:39 +0330

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+# avalin codet to python
+print("hello-world")
+
+# sakhtan moteghayer(variable)
+sadra = 8
+mahdy = 16
+
+#sakhtan moteghayer ha
+print(mahdy)
+print(sadra)
+
+# jam krdn moteghayer ha
+print(sadra + mahdy)
+
+#tarif krdn ye sharte mosbat ba moteghayer ha
+ass = True
+
+if ass is True:
+    print("koon")
+
+#tarif kardn shart ba halate manfi
+apple = False
+
+if apple is True:
+    print("sib")
+
+
+#tarif ye shart ba halate manfi va mosbat
+if apple is True:
+    print("OK")
+else:
+    print("NO")
